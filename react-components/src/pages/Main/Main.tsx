396973eba5e0037275c255121fc2a16db26c8b17
Bella Makhova <rgvder@gmail.com>
2022-10-19 23:01:18 +0700

    init: start React.API

--- react-components/src/pages/Main/Main.tsx
+++ react-components/src/pages/Main/Main.tsx
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
-import SearchBar from '../../components/main/SearchBar/SearchBar';
-import Catalog from '../../components/main/Catalog/Catalog';
-import items from '../../assets/source/items.json';
-import { Item } from '../../models/Item.interface';
 
-class Main extends Component<Record<string, never>, { items: Item[] }> {
-  state = {
-    items: items,
-  };
+const BASE_PATH: string = 'https://rickandmortyapi.com/api';
+const SEARCH_PATH: string = '/search';
+const SEARCH_PARAM: string = 'query=';
 
-  filterItems: (query: string) => void = (query: string) => {
-    this.setState({
-      items: [
-        ...items.filter(
-          (item: Item) => !query || item.model.toLowerCase().includes(query.toLowerCase())
-        ),
-      ],
-    });
-  };
+class Main extends Component<Record<string, never>, Record<string, never> }> {
+
+
 
   render() {
     return (
       <>
-        <SearchBar filterItems={this.filterItems} />
-        {this.state.items.length ? (
-          <Catalog items={this.state.items} />
-        ) : (
-          <p className="text">Nothing was found according to your request.</p>
-        )}
+        {/*<SearchBar />*/}
       </>
     );
   }
