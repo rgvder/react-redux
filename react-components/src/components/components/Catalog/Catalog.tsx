701967920ef147425c5e6d494754ba469ae6d72d
Bella Makhova <rgvder@gmail.com>
2022-11-01 16:17:39 +0700

    feat: add hooks to React.Components task

--- react-components/src/components/components/Catalog/Catalog.tsx
+++ react-components/src/components/components/Catalog/Catalog.tsx
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Catalog.module.scss';
 import { Card } from '../Card/Card';
 import { Item } from '../../../models/Item.interface';
 
-class Catalog extends Component<{ items: Item[] }> {
-  render() {
-    return (
-      <section className={styles.catalog}>
-        <div>
-          {this.props.items &&
-            this.props.items.map((item) => (
-              <Card
-                key={item.id}
-                image={item.image}
-                brand={item.brand}
-                brandId={item.brandId}
-                model={item.model}
-                color={item.color}
-                colorId={item.colorId}
-                price={item.price}
-                rating={item.rating}
-                count={item.count}
-                suctionPower={item.suctionPower}
-                cleaningType={item.cleaningType}
-                cleaningTypeId={item.cleaningTypeId}
-                isPopular={item.isPopular}
-                id={item.id}
-              />
-            ))}
-        </div>
-      </section>
-    );
-  }
-}
+const Catalog = (props: { items: Item[] }) => {
+  return (
+    <section className={styles.catalog}>
+      <div>
+        {props.items &&
+          props.items.map((item) => (
+            <Card
+              key={item.id}
+              image={item.image}
+              brand={item.brand}
+              brandId={item.brandId}
+              model={item.model}
+              color={item.color}
+              colorId={item.colorId}
+              price={item.price}
+              rating={item.rating}
+              count={item.count}
+              suctionPower={item.suctionPower}
+              cleaningType={item.cleaningType}
+              cleaningTypeId={item.cleaningTypeId}
+              isPopular={item.isPopular}
+              id={item.id}
+            />
+          ))}
+      </div>
+    </section>
+  );
+};
 
 export default Catalog;
--- react-components/src/pages/Components/Components.tsx
+++ react-components/src/pages/Components/Components.tsx
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import SearchBar from '../../components/components/SearchBar/SearchBar';
 import Catalog from '../../components/components/Catalog/Catalog';
 import items from '../../assets/source/items.json';
 import { Item } from '../../models/Item.interface';
 
-class Components extends Component<Record<string, never>, { items: Item[] }> {
-  state = {
-    items: items,
+const useComponents = (props: Item[]) => {
+  const [state, setState] = useState(props);
+
+  const filterItems: (query: string) => void = (query: string) => {
+    setState([
+      ...props.filter(
+        (item: Item) => !query || item.model.toLowerCase().includes(query.toLowerCase())
+      ),
+    ]);
   };
 
-  filterItems: (query: string) => void = (query: string) => {
-    this.setState({
-      items: [
-        ...items.filter(
-          (item: Item) => !query || item.model.toLowerCase().includes(query.toLowerCase())
-        ),
-      ],
-    });
-  };
+  return { state, filterItems };
+};
 
-  render() {
-    return (
-      <>
-        <SearchBar filterItems={this.filterItems} />
-        {this.state.items.length ? (
-          <Catalog items={this.state.items} />
-        ) : (
-          <p className="text">Nothing was found according to your request.</p>
-        )}
-      </>
-    );
-  }
-}
+const Components = () => {
+  const { state, filterItems } = useComponents(items);
+
+  return (
+    <>
+      <SearchBar filterItems={filterItems} />
+      {state.length ? (
+        <Catalog items={state} />
+      ) : (
+        <p className="text">Nothing was found according to your request.</p>
+      )}
+    </>
+  );
+};
 
 export default Components;
